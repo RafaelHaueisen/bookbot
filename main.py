625cb2badbead7ea54333fb0652a375181b55c8e
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-08-02 11:11:18 -0300

    Print book stats on console

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
     num_words = get_num_words(text)
-    print("The text has", num_words, "words.")
 
     # Create chars dict
     lower_text = text.lower()
     num_chars = count_chars(lower_text)
     print("The text has this number of chars", num_chars)
+
+    # Final output
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(num_words, " words found in the document\n")
+    char_values = sorted_dict(num_chars)
+    for i in range(len(char_values)):
+        if char_values[i][0].isalpha():
+            print(f"The {char_values[i][0]} character was found {char_values[i][1]} times")
+    print("--- End report ---")
+
+def sorted_dict(dict):
+    sorted_items = sorted(dict.items(), key=lambda item: item[1], reverse=True)
+    return sorted_items
 
 def count_chars(text):
     chars_dict = {}
