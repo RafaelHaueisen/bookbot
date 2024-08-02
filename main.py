1edc0a40ff108a0c236190f2ece5fcb1f522fe6b
RafaelHaueisen <rafaelhaueisenb@gmail.com>
2024-08-02 09:15:10 -0300

    Count text words

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    num_words = get_num_words(text)
+    print("The tex has", num_words, "words.")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
 main()
